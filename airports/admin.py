45c8f0f3e01568f571778d7815dbb12b810ca633
NazarHumen <nazargumen172@gmail.com>
2026-05-31 23:09:12 +0300

    refactor(airports): remove Ticket model

--- airports/admin.py
+++ airports/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Airline, Airplane, Airport, Country, Flight, Ticket
+from .models import Airline, Airplane, Airport, Country, Flight
 
 admin.site.register(Country)
 admin.site.register(Airport)
 admin.site.register(Airline)
 admin.site.register(Airplane)
 admin.site.register(Flight)
-admin.site.register(Ticket)
