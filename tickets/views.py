d98c480cb505204bede147c5d32c4616f2ad47c0
NazarHumen <nazargumen172@gmail.com>
2026-06-02 17:13:00 +0300

    feat(tickets): tag viewset for OpenAPI grouping

--- tickets/views.py
+++ tickets/views.py
@@ -1,9 +1,11 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 
 from tickets.models import Ticket
 from tickets.serializers import TicketSerializer
 
 
+@extend_schema(tags=["Tickets"])
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.select_related("flight", "user")
     serializer_class = TicketSerializer
