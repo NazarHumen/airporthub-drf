d77db04b459cb1d5d5ec36f7136343e9fa613593
NazarHumen <nazargumen172@gmail.com>
2026-06-07 19:16:06 +0300

    feat(tickets): scope ticket queryset to owner and force owner on create

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,9 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from tickets.models import Ticket
+from tickets.permissions import IsOwnerOrAdmin
 from tickets.serializers import TicketSerializer
 
 
@@ -9,4 +11,16 @@ from tickets.serializers import TicketSerializer
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.select_related("flight", "user")
     serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
     filterset_fields = ["flight", "user", "status"]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if getattr(self, "swagger_fake_view", False):
+            return qs.none()
+        if self.request.user.is_admin:
+            return qs
+        return qs.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
