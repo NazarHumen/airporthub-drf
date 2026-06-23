1165ccc3d685ee0a035a5952b95281fc977c824e
NazarHumen <nazargumen172@gmail.com>
2026-06-23 18:16:28 +0300

    feat(orders): register orders router

---
+++ orders/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from orders.views import OrderViewSet
+
+app_name = "orders"
+
+router = DefaultRouter()
+router.register("orders", OrderViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
